2b54d07db700fe5ced930a7d6dadf2a55906c510
Jeroen Engels <jfm.engels@gmail.com>
2019-08-28 19:28:12 +0200

    Add NoUnused rules to the lint config

--- lint/LintConfig.elm
+++ lint/LintConfig.elm
@@ -11,9 +11,13 @@ To add packages that contain rules, add them to this lint project using
 
 import Lint.Rule exposing (Rule)
 import NoDebug
+import NoUnused.CustomTypeConstructors
+import NoUnused.Variables
 
 
 config : List Rule
 config =
     [ NoDebug.rule
+    , NoUnused.Variables.rule
+    , NoUnused.CustomTypeConstructors.rule
     ]
